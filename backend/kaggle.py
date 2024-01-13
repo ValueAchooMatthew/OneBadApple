09af3af3815564ff6e89b6a413652b3524c62e6e
Kelvin Yu <yukaiwenn@gmail.com>
2024-01-13 15:01:45 -0500

    dataset uploaded

--- backend/kaggle.py
+++ backend/kaggle.py
@@ -12,16 +12,16 @@ get the copy API command from kaggle
 """
 
 # change the dataset name based on the dataset you want to download
-dataset_name = 'bobbyscience/league-of-legends-diamond-ranked-games-10-min'
+dataset_name = 'sriramr/fruits-fresh-and-rotten-for-classification'
 
-command = f'kaggle datasets download -d {dataset_name} -p dataset/images'
+command = f'kaggle datasets download -d {dataset_name} -p dataset-download/images'
 
 # run the kaggle command to download the file
 subprocess.run(command, shell=True)
 
 # unzip the downloaded file
-zip_file_path = os.path.join('dataset', 'images', f'{dataset_name.split("/")[1]}.zip')
-output_dir = os.path.join('dataset', 'images')
+zip_file_path = os.path.join('dataset-download', 'images', f'{dataset_name.split("/")[1]}.zip')
+output_dir = os.path.join('dataset-download', 'images')
 
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall(output_dir)
