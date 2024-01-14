5bc64135d010871875a04f5b2f2ebb365eb43b7d
Kelvin Yu <yukaiwenn@gmail.com>
2024-01-13 20:43:50 -0500

    working model

--- backend/train.py
+++ backend/train.py
@@ -1,11 +1,14 @@
 from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
 
 base_model = keras.applications.VGG16(
     weights='imagenet',
     input_shape=(224, 224, 3),
     include_top=False)
 
-num_classes=6
+num_classes = 6
 # Create inputs with correct shape
 inputs = keras.Input(shape=(224, 224, 3))
 
@@ -15,18 +18,16 @@ x = base_model(inputs, training=False)
 x = keras.layers.GlobalAveragePooling2D()(x)
 
 # Add final dense layer
-outputs = keras.layers.Dense(num_classes, activation = 'softmax')(x)
+outputs = keras.layers.Dense(num_classes, activation='softmax')(x)
 
 # Combine inputs and outputs to create model
-model = keras.Model(inputs,outputs)
+model = keras.Model(inputs, outputs)
 
 model.summary()
 
 base_model.trainable = True
-model.compile(optimizer=keras.optimizers.RMSprop(learning_rate = .00001),
-             loss='categorical_crossentropy', metrics=['accuracy'])
-
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.00001),
+                loss='categorical_crossentropy', metrics=['accuracy'])
 
 datagen_train = ImageDataGenerator(
     samplewise_center=True,
@@ -34,7 +35,9 @@ datagen_train = ImageDataGenerator(
     zoom_range=0.1,
     width_shift_range=0.1,
     height_shift_range=0.1,
-    horizontal_flip=True, )
+    horizontal_flip=True,
+)
+
 datagen_valid = ImageDataGenerator(
     samplewise_center=True,
     rotation_range=15,
@@ -42,7 +45,8 @@ datagen_valid = ImageDataGenerator(
     width_shift_range=0.1,
     height_shift_range=0.1,
     horizontal_flip=True,
-    vertical_flip=False,)
+    vertical_flip=False,
+)
 
 train_path = './backend/dataset-download/images/dataset/dataset/test'
 valid_path = './backend/dataset-download/images/dataset/dataset/train'
@@ -62,8 +66,14 @@ valid_it = datagen_valid.flow_from_directory(
     class_mode="categorical",
 )
 
+
+
 model.fit(train_it,
-          validation_data=valid_it,
-          steps_per_epoch=len(train_it),  
-          validation_steps=len(valid_it),  
-          epochs=1)
+            validation_data=valid_it,
+            steps_per_epoch=len(train_it),
+            validation_steps=len(valid_it),
+            epochs=2)
+
+print("done train")
+model.save("bad_apple1.keras")
+print("done save")
