e7bb14af29aa1bec48885153f6c382e88ffaf3b0
Kelvin Yu <yukaiwenn@gmail.com>
2024-01-13 23:15:59 -0500

    updated files

---
+++ backend/test.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import os
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+from keras.preprocessing import image as image_utils
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+model = load_model('bad_apple1.keras')
+
+image_path = r'C:\Users\yukai\Desktop\coding-stuff\personal-projects\OneBadApple\backend\uploaded_image.jpg'
+
+def show_image(image_path):
+    image = mpimg.imread(image_path)
+    print(image.shape)
+    plt.imshow(image)
+
+
+show_image(image_path)
+image = image_utils.load_img(image_path, target_size=(224, 224))
+image = image_utils.img_to_array(image)
+image = image.reshape(1, 224, 224, 3)
+image = preprocess_input(image)
+show_image(image_path)
+
+preds = model.predict(image)
+
+print(preds)
