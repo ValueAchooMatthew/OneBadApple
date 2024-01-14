825eeb54630d2a8261b6d00750a169d5a9adf50c
Matthew <matthewtamerfarah@gmail.com>
2024-01-13 22:16:04 -0500

    added backend image processing

--- frontend/app/(routes)/page.tsx
+++ frontend/app/(routes)/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
       <h1 className="text-5xl text-center font-bold text-grey-950">
         One Bad Apple
       </h1>
-      <h3 className="text-center text-sm mt-3 italic">
+      <h3 className="text-center text-sm mt-5 italic">
         Addressing the United Nation's call to action for good health and wellbeing, one meal at a time.
       </h3>
-      <div className="flex justify-center text-2xl mt-40 font-semibold text-white">
-        <Link href={"/food-checker"} className="px-3 py-1 rounded-md bg-blue-300 mr-12">
+      <div className="flex justify-center text-2xl mt-32 font-semibold text-white">
+        <Link href={"/food-checker"} className="px-3 py-1 rounded-md bg-[#0099CA] hover:bg-[#0099FA] mr-12 hover:-translate-y-4 transition-all duration-300">
           Check Food Health
         </Link>
-        <Link href={"/mission-statement"} className="px-3 py-1 rounded-md bg-blue-300 ml-12">
+        <Link href={"/mission-statement"} className="px-3 py-1 rounded-md bg-[#0099CA] hover:bg-[#0099FA] ml-12 hover:-translate-y-4 transition-all duration-300">
           <span>
             Mission Statement
           </span>
